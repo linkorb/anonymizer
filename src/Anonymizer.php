54c67de8d64cd35bf1e1bd180dc2e4a7608adeaa
j.faassen <j.faassen@linkorb.com>
2017-09-30 22:54:16 +0200

    Support tablename wildcards

--- src/Anonymizer.php
+++ src/Anonymizer.php
@@ -26,8 +26,52 @@ class Anonymizer
         $this->columns = new TypedArray(Column::class);
     }
 
+    private function loadSchema(PDO $pdo, OutputInterface $output)
+    {
+        $stmt = $pdo->prepare("SHOW tables;");
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+            $tableName = $row[0];
+            $this->schema[$tableName]=[];
+        }
+        foreach ($this->schema as $tableName => $columns) {
+            $stmt = $pdo->prepare("describe " . $tableName);
+            $stmt->execute();
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $fieldName = $row['Field'];
+                $this->schema[$tableName][$fieldName] = [
+                    'type' => $row['Type'],
+                    'null' => $row['Null'],
+                    'default' => $row['Default'],
+                    'extra' => $row['Extra']
+                ];
+            }
+        }
+
+        // foreach ($this->schema as $tableName => $columns) {
+        //     foreach ($columns as $name=>$details) {
+        //         echo $tableName . '.' . $name . "\n";
+        //     }
+        // }
+        //exit();
+        //print_r($this->schema); exit("DONE");
+    }
+
+    public function expandTables($pattern)
+    {
+        $tableNames = [];
+        foreach ($this->schema as $tableName => $columns) {
+            if (fnmatch($pattern, $tableName)) {
+                $tableNames[] = $tableName;
+            }
+        }
+        return $tableNames;
+    }
+
     public function execute(PDO $pdo, OutputInterface $output)
     {
+        $this->loadSchema($pdo, $output);
+
         foreach ($this->columns as $column) {
             $output->writeLn("Anonymizing column: <info>" . $column->identifier() . "</info> ({$column->displayMethod()})");
             $method = new \Anonymizer\Method\FakerMethod($column->getArguments());
@@ -136,23 +180,32 @@ class Anonymizer
 
         foreach ($this->drop as $drop) {
             $part = explode('.', $drop);
-            $tableName = $part[0];
-            if (count($part)==1) {
-                $output->writeLn("Dropping table: <info>{$tableName}</info>");
+            //print_r($part);
+            $tableNames = $this->expandTables($part[0]);
+            foreach ($tableNames as $tableName) {
+                switch (count($part)) {
+                    case 1:
+                        $output->writeLn("Dropping table: <info>{$tableName}</info>");
 
-                $subStmt = $pdo->prepare(
-                    "DROP TABLE " . $tableName . ';'
-                );
-                $subStmt->execute();
-            }
-            if (count($part)==2) {
-                $columnName = $part[1];
-                $output->writeLn("Dropping column: <info>{$tableName}.{$columnName}</info>");
+                        $subStmt = $pdo->prepare(
+                            "DROP TABLE " . $tableName . ';'
+                        );
+                        $subStmt->execute();
+                        break;
+                    case 2:
+                        $columnName = (string)$part[1];
+                        if (isset($this->schema[$tableName][$columnName])) {
+                            $output->writeLn("Dropping column: <info>{$tableName}.{$columnName}</info>");
 
-                $subStmt = $pdo->prepare(
-                    "ALTER TABLE " . $tableName . ' DROP COLUMN ' . $columnName
-                );
-                $subStmt->execute();
+                            $subStmt = $pdo->prepare(
+                                "ALTER TABLE " . $tableName . ' DROP COLUMN ' . $columnName
+                            );
+                            $subStmt->execute();
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("Unexpected part count: " . count($part));
+                }
             }
         }
 
